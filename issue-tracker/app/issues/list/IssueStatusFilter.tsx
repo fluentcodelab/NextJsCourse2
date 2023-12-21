35a8ba8d3685b42192d80db1a62f411aac163dd8
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-12-21 14:28:36 -0500

    Filter issues

--- issue-tracker/app/issues/list/IssueStatusFilter.tsx
+++ issue-tracker/app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Select } from "@radix-ui/themes";
 import { Status } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 const statuses: { label: string; value: Status | "ALL" }[] = [
   { label: "All", value: "ALL" },
@@ -11,8 +12,15 @@ const statuses: { label: string; value: Status | "ALL" }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        status = status === "ALL" ? "" : status;
+        const query = status ? `?status=${status}` : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
