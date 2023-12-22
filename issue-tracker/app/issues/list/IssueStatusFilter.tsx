36c56bd5af94278f8419fbf639143a179dd3b01e
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-12-22 07:40:51 -0500

    Fix filtering bugs

--- issue-tracker/app/issues/list/IssueStatusFilter.tsx
+++ issue-tracker/app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Select } from "@radix-ui/themes";
 import { Status } from "@prisma/client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value: Status | "ALL" }[] = [
   { label: "All", value: "ALL" },
@@ -13,11 +13,20 @@ const statuses: { label: string; value: Status | "ALL" }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
         status = status === "ALL" ? "" : status;
-        const query = status ? `?status=${status}` : "";
+
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >
